d9b736524257ab0d2f56a8557dd0fed31ffb0faa
Rocco Meli <r.meli@bluemail.ch>
2021-04-01 13:09:09 +0200

    revert change in noxfile

--- noxfile.py
+++ noxfile.py
@@ -19,10 +19,7 @@ def tests(session: Session) -> None:
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(
-        session,
-        "coverage[toml]",
-        "pytest",
-        "pytest-cov",
+        session, "coverage[toml]", "pytest", "pytest-cov",
     )
     session.run("pytest", *args)
 
